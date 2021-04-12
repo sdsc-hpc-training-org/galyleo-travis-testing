9d394927a3eb8d03d06a6599757478e5c7b183f1
JamesMcDougallJr <jmcdouga@ucsd.edu>
2021-04-12 16:31:29 -0700

    Remove comet tests for now

--- tests.sh
+++ tests.sh
@@ -11,5 +11,5 @@ cometCmd="/home/jamesmcd/Code/reverse-proxy/start-jupyter"
 test0="$expanseCmd -A ddp363"
 test1="$expanseCmd -A ddp363 --gpus 1 -p gpu-shared -e singularitypro -s /cm/shared/apps/containers/singularity/tensorflow/tensorflow-2.3.0-gpu-20200929.simg"
 
-test2="$cometCmd -s notebook"
-test3="$cometCmd -s jupyterlab"
+#test2="$cometCmd -s notebook"
+#test3="$cometCmd -s jupyterlab"
